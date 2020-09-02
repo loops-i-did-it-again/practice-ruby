964a59eed57b4ac2b4b004bf06e7931ff61b1501
dani <dani.zaghian@gmail.com>
2020-09-02 09:53:32 -0700

    adds employee as a class

--- employee.rb
+++ employee.rb
@@ -6,13 +6,64 @@
 
 # hash
 # string hashrocket syntax
-employee1 = {"first_name" => "Jay", "last_name" => "Wengrow", "salary" => 150000, "active" => true}
-# symbol syntax
-employee2 = {:last_name => "Zaghian", :salary => 40000, :active => true, :first_name => "Dani"}
-# js syntax (keys are symbols)
-employee3 = {first_name: "Carl", last_name: "Hauck", salary: 0, active: true}
+# employee1 = {"first_name" => "Jay", "last_name" => "Wengrow", "salary" => 150000, "active" => true}
+# # symbol syntax
+# employee2 = {:last_name => "Zaghian", :salary => 40000, :active => true, :first_name => "Dani"}
+# # js syntax (keys are symbols)
+# employee3 = {first_name: "Carl", last_name: "Hauck", salary: 0, active: true}
 
-puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
+# puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
 
 # class
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    # instance variables
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  # getter/reader (instance method)
+  def first_name
+    @first_name
+  end
+  
+  def last_name
+    @last_name
+  end
+
+  def salary
+    @salary
+  end
+
+  def active
+    @active
+  end
+
+  # setter/writer (instance methods)
+  def active=(input_active)
+    @active = input_active
+  end
+
+  # instance method
+  def print_info
+    puts "#{first_name} #{last_name} makes #{salary} a year."
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+end
+
+employee1 = Employee.new("Jay", "Wengrow", 150000, true)
+employee2 = Employee.new("Dani", "Zaghian", 40000, true)
+employee1.print_info
+employee2.print_info
+puts employee2.give_annual_raise
+puts employee2.first_name
+employee1.active = false
+puts employee1.active
+
+
