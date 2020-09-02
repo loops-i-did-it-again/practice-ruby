221437604003b2cb84f747b82c13b1619511db1e
dani <dani.zaghian@gmail.com>
2020-09-02 12:11:14 -0700

    fixes inheritance example

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,5 @@
 class Vehicle
-  def initialize
+  def initialize(input_options)
     @speed = 0
     @direction = 'north'
   end
@@ -18,19 +18,34 @@ class Vehicle
 end
 
 class Car < Vehicle
+  attr_accessor :make, :model
+  def initialize(input_options)
+    super
+    @make = input_options[:make]
+    @model = input_options[:model]
+    @year = input_options[:year]
+  end
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  attr_accessor :type, :weight
+  def initialize(input_options)
+    super
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+  end
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-bike = Bike.new
-car = Car.new
+bike = Bike.new(type: "Mountain", weight: 25)
+car = Car.new(make: "Honda", model: "Civic", year: "2015")
 
 bike.ring_bell
+puts bike.type
 car.honk_horn
+puts car.make
